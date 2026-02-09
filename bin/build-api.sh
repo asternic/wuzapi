961cf3b3c61d0c25fdba0d2617577d44014071b5
Jefferson Felix <me@jeffersonfelix.dev>
2026-02-08 23:32:38 -0300

    release: v202602082330

--- bin/build-api.sh
+++ bin/build-api.sh
@@ -2,7 +2,7 @@
 
 # Load environment variables
 set -a; . ./.env; set +a
-export VERSION=v202602082315
+export VERSION=v202602082330
 
 # Build and push the image to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 058264349674.dkr.ecr.us-east-1.amazonaws.com
